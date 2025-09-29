a5d8bfc1e9c0733bf7c109f2f1c3448db8454392
bitbyte08 <me@bitworkspace.kr>
2025-09-29 19:53:10 +0900

    feat :: 프로젝트 생성

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = 'echo_slam'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+     data_files=[
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bitbyte08',
+    maintainer_email='me@bitworkspace.kr',
+    description='SLAM launch package',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
