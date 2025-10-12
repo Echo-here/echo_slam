0ffa23212121d5141cd67f23c4b9989ba5a025a2
bitbyte08 <me@bitworkspace.kr>
2025-10-13 07:24:37 +0900

    feat :: config file 적용

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -1,21 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('echo_slam')
+    slam_params_file = os.path.join(pkg_share, 'config', 'slam_params.yaml')
     return LaunchDescription([
         Node(
             package='slam_toolbox',
             executable='async_slam_toolbox_node',
             name='slam_toolbox',
             output='screen',
-            parameters=[
-                {
-                    'use_sim_time': False,
-                    'map_frame': 'map',
-                    'odom_frame':  'odom',
-                    'base_frame': 'base_link',
-                    'scan_topic': '/scan'
-                }
-            ]
+            parameters=[slam_params_file]
         )
     ])
