a10e4745b06b85ac85aeb39fc031d040108a241c
bitbyte08 <me@bitworkspace.kr>
2025-10-13 19:50:07 +0900

    feat :: nav2 추가

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -6,6 +6,8 @@ import os
 def generate_launch_description():
     pkg_share = get_package_share_directory('echo_slam')
     slam_params_file = os.path.join(pkg_share, 'config', 'slam_params.yaml')
+    nav2_params_file = os.path.join(pkg_share, 'config', 'nav2_params.yaml')
+
     return LaunchDescription([
         Node(
             package='slam_toolbox',
@@ -13,5 +15,39 @@ def generate_launch_description():
             name='slam_toolbox',
             output='screen',
             parameters=[slam_params_file]
+        ),
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server',
+            output='screen',
+            parameters=[nav2_params_file]
+        ),
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[nav2_params_file]
+        ),
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=[nav2_params_file]
+        ),
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_navigation',
+            output='screen',
+            parameters=[{
+                'use_sim_time': True,
+                'autostart': True,
+                'node_names': ['controller_server',
+                               'planner_server',
+                               'bt_navigator']
+            }]
         )
     ])
