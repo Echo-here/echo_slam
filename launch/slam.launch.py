88e1d99ee417255d5da96bf371ac03eea29ff4fd
bitbyte08 <me@bitworkspace.kr>
2025-10-23 19:07:10 +0900

    fix :: use_sim_time False

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -52,7 +52,7 @@ def generate_launch_description():
             name='lifecycle_manager_navigation',
             output='screen',
             parameters=[{
-                'use_sim_time': True,
+                'use_sim_time': False,
                 'autostart': True,
                 'node_names': ['controller_server', 'planner_server', 'bt_navigator']
             }]
