fbcf19de67b16103f8a274e9624c2156cc22e239
bitbyte08 <me@bitworkspace.kr>
2025-10-29 19:05:56 +0900

    fix :: params

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -43,7 +43,7 @@ def generate_launch_description():
             ]),
             launch_arguments={
                 "use_sim_time": "False",
-                "map": ""
+                "map": "/"
             }.items()
         ),
         Node(
